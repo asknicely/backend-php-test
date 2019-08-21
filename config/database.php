63f4eb9b6395205012bb75c3dc2ad3549a410055
曹博闻 <piaomiaoaaa@sina.com>
2019-08-21 13:11:41 +1200

    task 6 orm plugin

--- config/database.php
+++ config/database.php
@@ -2,9 +2,9 @@
 return [
     'driver'    => 'mysql',
     'host'      => 'localhost',
-    'database'  => 'phptest',
+    'database'  => '<your-database>',
     'username'  => 'root',
-    'password'  => 'qimingxing',
+    'password'  => '<your-password>',
     'charset'   => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',    //utf8mb4 is a better collation than utf8, if mysql version > 5.5.3
     'prefix'    => ''
