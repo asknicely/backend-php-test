a5c9202420461a499120af4365b8f3ff89ea9bb4
trackerapi <rflorentino@vertere-gs.com>
2019-09-29 13:46:03 +0800

    TASK-3 As a user I can view a todo in a JSON format. I added another function inside the controller plus another html in teamplates to view the json format of a todo. On click it will redirect to that page and will show the JSON format of the targeted todo. - Main Solution for Task 3

--- src/controllers.php
+++ src/controllers.php
@@ -66,6 +66,30 @@ $app->get('/todo/{id}', function ($id) use ($app) {
 })
 ->value('id', null);
 
+$app->get('/todo/{id}/json', function ($id) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+
+    if ($id){
+        $sql = "SELECT * FROM todos WHERE id = '$id'";
+        $todo = $app['db']->fetchAssoc($sql);
+        $jsontodo = json_encode($todo);
+        return $app['twig']->render('jsonformat.html', [
+            'type' => 'json', 'jsonformat' => $jsontodo,
+        ]);
+    } else {
+
+        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
+        $todos = $app['db']->fetchAll($sql);
+        $jsontodo = json_encode($todo);
+        return $app['twig']->render('jsonformat.html', [
+            'type' => 'json', 'jsonformat' => $jsontodo,
+        ]);
+    }
+})
+->value('id', null);
+
 
 $app->post('/todo/add', function (Request $request) use ($app) {
     if (null === $user = $app['session']->get('user')) {
