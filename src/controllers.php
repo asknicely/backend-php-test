35661c5a5a482aae9647ff549bbfced66015d44c
Michael Kerr <mike@onlysport.co.nz>
2019-10-06 02:32:11 +1300

    TASK-1

--- src/controllers.php
+++ src/controllers.php
@@ -5,6 +5,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     $twig->addGlobal('user', $app['session']->get('user'));
+    $twig->addGlobal('pageScripts', '');
 
     return $twig;
 }));
@@ -16,14 +17,16 @@ $app->get('/', function () use ($app) {
     ]);
 });
 
-
+/**
+ * Login action
+ */
 $app->match('/login', function (Request $request) use ($app) {
     $username = $request->get('username');
     $password = $request->get('password');
 
     if ($username) {
-        $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
-        $user = $app['db']->fetchAssoc($sql);
+        $sql = "SELECT * FROM users WHERE username = ? and password = ?";
+        $user = $app['db']->fetchAssoc($sql, [$username, $password]);
 
         if ($user){
             $app['session']->set('user', $user);
@@ -35,36 +38,58 @@ $app->match('/login', function (Request $request) use ($app) {
 });
 
 
+/**
+ * Logout action
+ */
 $app->get('/logout', function () use ($app) {
     $app['session']->set('user', null);
     return $app->redirect('/');
 });
 
 
-$app->get('/todo/{id}', function ($id) use ($app) {
+/**
+ * Todo list, detail, and json display
+ */
+$app->get('/todo/{id}/{json_flag}', function ($id, $json_flag) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
 
-    if ($id){
-        $sql = "SELECT * FROM todos WHERE id = '$id'";
-        $todo = $app['db']->fetchAssoc($sql);
+	// grab any error messages from session
+	$error_message = $app['session']->get('error_message') ?: '';
+	$app['session']->set('error_message', null);
 
-        return $app['twig']->render('todo.html', [
-            'todo' => $todo,
-        ]);
-    } else {
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
-        $todos = $app['db']->fetchAll($sql);
+	// if a non empty id has been supplied then load and display the todo
+    if ($id){
+        $sql = "SELECT * FROM todos WHERE id = ?";
+        $todo = $app['db']->fetchAssoc($sql, [$id]);
+
+		// if the 'json_flag' was set, then return the page content as an encoded json string
+		if ($json_flag) {
+			return json_encode($todo);
+		}
+		else {
+			return $app['twig']->render('todo.html', [
+				'todo' => $todo,
+				'error_message' => $error_message,
+			]);
+		}
+    } else {		
+        $sql = "SELECT * FROM todos WHERE user_id = ?";
+        $todos = $app['db']->fetchAll($sql, [$user['id']]);
 
         return $app['twig']->render('todos.html', [
             'todos' => $todos,
+			'error_message' => $error_message,
         ]);
     }
 })
-->value('id', null);
+->value('id', null)->value('json_flag', null);
 
 
+/**
+ * Todo addition action
+ */
 $app->post('/todo/add', function (Request $request) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
@@ -72,18 +97,25 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 
     $user_id = $user['id'];
     $description = $request->get('description');
-
-    $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
-    $app['db']->executeUpdate($sql);
+	
+	if (empty(trim($description))) {
+		$app['session']->set('error_message', 'Please enter a description');
+	}
+	else {
+		$sql = "INSERT INTO todos (user_id, description) VALUES (?, ?)";
+		$app['db']->executeUpdate($sql, [$user_id, $description]);
+	}
 
     return $app->redirect('/todo');
 });
 
 
+/**
+ * Todo deletion action
+ */
 $app->match('/todo/delete/{id}', function ($id) use ($app) {
-
-    $sql = "DELETE FROM todos WHERE id = '$id'";
-    $app['db']->executeUpdate($sql);
+    $sql = "DELETE FROM todos WHERE id = ?";
+    $app['db']->executeUpdate($sql, [$id]);
 
     return $app->redirect('/todo');
 });
