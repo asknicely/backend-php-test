b9ca4d5dafb45efe95a290c54297d457b598708c
Ricky Li <nzrickyli@gmail.com>
2019-10-29 15:00:25 +1300

    TASK-2: add complete a todo action

--- src/controllers.php
+++ src/controllers.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints as Assert;
 
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+$app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
     $twig->addGlobal('user', $app['session']->get('user'));
 
     return $twig;
@@ -24,9 +24,9 @@ $app->match('/login', function (Request $request) use ($app) {
 
     if ($username) {
         $sql = "SELECT * FROM users WHERE username = ? and password = ?";
-        $user = $app['db']->fetchAssoc($sql,[$username, $password]);
+        $user = $app['db']->fetchAssoc($sql, [$username, $password]);
 
-        if ($user){
+        if ($user) {
             $app['session']->set('user', $user);
             return $app->redirect('/todo');
         }
@@ -47,23 +47,23 @@ $app->get('/todo/{id}', function ($id) use ($app) {
         return $app->redirect('/login');
     }
 
-    if ($id){
-        $sql = "SELECT * FROM todos WHERE id = '$id'";
-        $todo = $app['db']->fetchAssoc($sql);
+    if ($id) {
+        $sql = "SELECT * FROM todos WHERE id = ? AND user_id = ?";
+        $todo = $app['db']->fetchAssoc($sql, [$id, $user['id']]);
 
         return $app['twig']->render('todo.html', [
             'todo' => $todo,
         ]);
     } else {
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
-        $todos = $app['db']->fetchAll($sql);
+        $sql = "SELECT * FROM todos WHERE user_id = ?";
+        $todos = $app['db']->fetchAll($sql, [$user['id']]);
 
         return $app['twig']->render('todos.html', [
             'todos' => $todos,
         ]);
     }
 })
-->value('id', null);
+    ->value('id', null);
 
 
 $app->post('/todo/add', function (Request $request) use ($app) {
@@ -79,7 +79,7 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 
     if (count($errors) == 0) {
         $sql = "INSERT INTO todos (user_id, description) VALUES (?, ?)";
-        $app['db']->executeUpdate($sql,[$user_id, $description]);
+        $app['db']->executeUpdate($sql, [$user_id, $description]);
     }
 
     return $app->redirect('/todo');
@@ -87,9 +87,35 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 
 
 $app->match('/todo/delete/{id}', function ($id) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
 
-    $sql = "DELETE FROM todos WHERE id = '$id'";
-    $app['db']->executeUpdate($sql);
+    $sql = "DELETE FROM todos WHERE id = ? AND user_id = ?";
+    $app['db']->executeUpdate($sql, [$id, $user['id']]);
+
+    return $app->redirect('/todo');
+});
+
+// complete a todo
+$app->match('/todo/complete/{id}', function ($id) use ($app) {
+
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+
+    $sql = "SELECT * FROM todos WHERE id = ?";
+    $todo = $app['db']->fetchAssoc($sql, [$id]);
+
+    if ($todo) {
+        $sql = "UPDATE todos SET completed_at = ? WHERE id = ? AND user_id = ?";
+        $app['db']->executeUpdate($sql,
+            [
+                (new DateTime())->format('Y-m-d H:i:s'),
+                $id,
+                $user['id']
+            ]);
+    }
 
     return $app->redirect('/todo');
 });
