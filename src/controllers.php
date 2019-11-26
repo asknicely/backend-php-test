38fdb6672d9c95014279c6c9cde5e8abf94ec2f2
Muhammad Ali <muhammadali@Users-MacBook-Pro.local>
2019-11-27 12:45:08 +1300

    Task5: As a user I can see my list of todos paginated.

--- src/controllers.php
+++ src/controllers.php
@@ -21,23 +21,18 @@ $app->get('/', function () use ($app) {
 $app->match('/login', function (Request $request) use ($app) {
     $username = $request->get('username');
     $password = md5($request->get('password'));
+
     if ($username) {
         $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
         $user = $app['db']->fetchAssoc($sql);
-        if ($user){
 
+        if ($user){
             $app['session']->set('user', $user);
             return $app->redirect('/todo');
-
-        }else{
-
-        $app['session']->getFlashBag()->add('error_msg', 'Sorry username or password is incorrect. Please try again.');
-
         }
-         
-        }
-        return $app['twig']->render('login.html', array());    
-   
+    }
+
+    return $app['twig']->render('login.html', array());
 });
 
 
@@ -51,8 +46,8 @@ $app->get('/todo/{id}', function ($id) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
-    $user    = $app['session']->get('user');
     $user_id = $user['id'];
+
     if ($id){
         
         $sql = "SELECT * FROM todos WHERE id = '$id'";
@@ -62,11 +57,30 @@ $app->get('/todo/{id}', function ($id) use ($app) {
             'todo' => $todo,
         ]);
     } else {
- 
+
         $sql = "SELECT * FROM todos WHERE user_id = '$user_id'";
         $todos = $app['db']->fetchAll($sql);
+        $number_of_results = count($todos);
+        $results_per_page = 5;
+        $number_of_pages = ceil($number_of_results/$results_per_page);
+       // $page = $_GET['page'];
+
+        if(!isset($_GET['page'])){
+            $page = 1;
+
+        }else{
+
+            $page = $_GET['page'];
+        }
+
+        $offset = ($page-1)*$results_per_page;
+
+        $sql = "SELECT * FROM todos WHERE user_id = '$user_id' LIMIT $offset,$results_per_page";
+        $todos = $app['db']->fetchAll($sql);
+        //echo $page;exit;
         return $app['twig']->render('todos.html', [
             'todos' => $todos,
+            'number_of_pages' => $number_of_pages,
         ]);
     }
 })
@@ -119,3 +133,72 @@ $app->match('/todo/delete/{id}', function ($id) use ($app) {
     $app['session']->getFlashBag()->add('success_msg', 'The task has been deleted successfully.');
     return $app->redirect('/todo');
 });
+
+$app->match('/todo/edit/{id}', function ($id) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+
+    $user    = $app['session']->get('user');
+    $user_id = $user['id'];
+
+    $sql = "SELECT * FROM todos WHERE id = '$id'";
+    $todo = $app['db']->fetchAssoc($sql);
+
+   // print_r($todo);exit;
+
+    return $app['twig']->render('edit.html', [
+            'todo' => $todo,
+    ]);
+
+});
+
+$app->post('/todo/update', function (Request $request) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+
+    $errors = $app['validator']->validate($request->get('description'), new Validator\NotBlank());
+
+    if (count($errors) == 0) {
+
+    $user_id = $user['id'];
+    $description = $request->get('description');
+    $id = $request->get('id');
+    $sql = "UPDATE todos SET description = '$description' WHERE id= '$id'";
+    $app['db']->executeUpdate($sql);
+
+    # Set flash message and return redirect
+    $app['session']->getFlashBag()->add('success_msg', 'The task has been updated successfully.');
+    return $app->redirect('/todo');
+    
+    }
+    else{
+
+    # Set flash message and return redirect
+    $app['session']->getFlashBag()->add('error_msg', 'Description field is required.');
+    return $app->redirect('/todo');
+
+    }
+    
+
+});
+
+$app->match('/todo/task_complete/{id}', function ($id) use ($app) {
+
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+
+    $sql = "UPDATE todos SET is_completed = 1 WHERE id= '$id'";
+    $app['db']->executeUpdate($sql);
+
+    # Set flash message and return redirect
+    $app['session']->getFlashBag()->add('success_msg', 'The task has been marked as completed successfully.');
+    return $app->redirect('/todo');
+    
+    
+
+});
+
+
