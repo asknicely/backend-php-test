b156efb7da629f4e9c64b82d068ffbaeadefd110
Muhammad Ali <muhammadali@Users-MacBook-Pro.local>
2019-11-27 12:48:25 +1300

    Task3: As a user I can view a todo in a JSON format.

--- src/controllers.php
+++ src/controllers.php
@@ -21,18 +21,23 @@ $app->get('/', function () use ($app) {
 $app->match('/login', function (Request $request) use ($app) {
     $username = $request->get('username');
     $password = md5($request->get('password'));
-
     if ($username) {
         $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
         $user = $app['db']->fetchAssoc($sql);
-
         if ($user){
+
             $app['session']->set('user', $user);
             return $app->redirect('/todo');
-        }
-    }
 
-    return $app['twig']->render('login.html', array());
+        }else{
+
+        $app['session']->getFlashBag()->add('error_msg', 'Sorry username or password is incorrect. Please try again.');
+
+        }
+         
+        }
+        return $app['twig']->render('login.html', array());    
+   
 });
 
 
@@ -46,18 +51,20 @@ $app->get('/todo/{id}', function ($id) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
-
+    $user    = $app['session']->get('user');
+    $user_id = $user['id'];
     if ($id){
         
         $sql = "SELECT * FROM todos WHERE id = '$id'";
         $todo = $app['db']->fetchAssoc($sql);
-
+        $todo_jason = json_encode($todo);
         return $app['twig']->render('todo.html', [
             'todo' => $todo,
+            'todo_jason' => $todo_jason,
         ]);
     } else {
-
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
+ 
+        $sql = "SELECT * FROM todos WHERE user_id = '$user_id'";
         $todos = $app['db']->fetchAll($sql);
         return $app['twig']->render('todos.html', [
             'todos' => $todos,
@@ -113,72 +120,3 @@ $app->match('/todo/delete/{id}', function ($id) use ($app) {
     $app['session']->getFlashBag()->add('success_msg', 'The task has been deleted successfully.');
     return $app->redirect('/todo');
 });
-
-$app->match('/todo/edit/{id}', function ($id) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
-        return $app->redirect('/login');
-    }
-
-    $user    = $app['session']->get('user');
-    $user_id = $user['id'];
-
-    $sql = "SELECT * FROM todos WHERE id = '$id'";
-    $todo = $app['db']->fetchAssoc($sql);
-
-   // print_r($todo);exit;
-
-    return $app['twig']->render('edit.html', [
-            'todo' => $todo,
-    ]);
-
-});
-
-$app->post('/todo/update', function (Request $request) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
-        return $app->redirect('/login');
-    }
-
-    $errors = $app['validator']->validate($request->get('description'), new Validator\NotBlank());
-
-    if (count($errors) == 0) {
-
-    $user_id = $user['id'];
-    $description = $request->get('description');
-    $id = $request->get('id');
-    $sql = "UPDATE todos SET description = '$description' WHERE id= '$id'";
-    $app['db']->executeUpdate($sql);
-
-    # Set flash message and return redirect
-    $app['session']->getFlashBag()->add('success_msg', 'The task has been updated successfully.');
-    return $app->redirect('/todo');
-    
-    }
-    else{
-
-    # Set flash message and return redirect
-    $app['session']->getFlashBag()->add('error_msg', 'Description field is required.');
-    return $app->redirect('/todo');
-
-    }
-    
-
-});
-
-$app->match('/todo/task_complete/{id}', function ($id) use ($app) {
-
-    if (null === $user = $app['session']->get('user')) {
-        return $app->redirect('/login');
-    }
-
-    $sql = "UPDATE todos SET is_completed = 1 WHERE id= '$id'";
-    $app['db']->executeUpdate($sql);
-
-    # Set flash message and return redirect
-    $app['session']->getFlashBag()->add('success_msg', 'The task has been marked as completed successfully.');
-    return $app->redirect('/todo');
-    
-    
-
-});
-
-
