25767ec278d4e99811f8adefeecb4accc734f4b4
Muhammad Ali <muhammadali@Users-MacBook-Pro.local>
2019-11-27 20:02:50 +1300

    Task2: As a user I can mark a todo as completed.

--- src/controllers.php
+++ src/controllers.php
@@ -1,32 +1,32 @@
 <?php
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints as Assert;
 
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+$app['twig'] = $app->share($app->extend('twig', function ($twig, $app)
+{
     $twig->addGlobal('user', $app['session']->get('user'));
 
     return $twig;
 }));
 
-
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', [
-        'readme' => file_get_contents('README.md'),
-    ]);
+$app->get('/', function () use ($app)
+{
+    return $app['twig']->render('index.html', ['readme' => file_get_contents('README.md') , ]);
 });
 
-
-$app->match('/login', function (Request $request) use ($app) {
+$app->match('/login', function (Request $request) use ($app)
+{
     $username = $request->get('username');
     $password = md5($request->get('password'));
 
-    if ($username) {
+    if ($username)
+    {
         $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
         $user = $app['db']->fetchAssoc($sql);
 
-        if ($user){
+        if ($user)
+        {
             $app['session']->set('user', $user);
             return $app->redirect('/todo');
         }
@@ -35,157 +35,151 @@ $app->match('/login', function (Request $request) use ($app) {
     return $app['twig']->render('login.html', array());
 });
 
-
-$app->get('/logout', function () use ($app) {
+$app->get('/logout', function () use ($app)
+{
     $app['session']->set('user', null);
     return $app->redirect('/');
 });
 
-
-$app->get('/todo/{id}', function ($id) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
+$app->get('/todo/{id}', function ($id) use ($app)
+{
+    if (null === $user = $app['session']->get('user'))
+    {
         return $app->redirect('/login');
     }
+    $user = $app['session']->get('user');
     $user_id = $user['id'];
 
-    if ($id){
-        
+    if ($id)
+    {
+
         $sql = "SELECT * FROM todos WHERE id = '$id'";
         $todo = $app['db']->fetchAssoc($sql);
 
-        return $app['twig']->render('todo.html', [
-            'todo' => $todo,
-        ]);
-    } else {
+        return $app['twig']->render('todo.html', ['todo' => $todo, ]);
+    }
+    else
+    {
 
         $sql = "SELECT * FROM todos WHERE user_id = '$user_id'";
         $todos = $app['db']->fetchAll($sql);
-        $number_of_results = count($todos);
-        $results_per_page = 5;
-        $number_of_pages = ceil($number_of_results/$results_per_page);
-       // $page = $_GET['page'];
-
-        if(!isset($_GET['page'])){
-            $page = 1;
-
-        }else{
-
-            $page = $_GET['page'];
-        }
-
-        $offset = ($page-1)*$results_per_page;
-
-        $sql = "SELECT * FROM todos WHERE user_id = '$user_id' LIMIT $offset,$results_per_page";
-        $todos = $app['db']->fetchAll($sql);
-        return $app['twig']->render('todos.html', [
-            'todos' => $todos,
-            'number_of_pages' => $number_of_pages,
-        ]);
+        return $app['twig']->render('todos.html', ['todos' => $todos, ]);
     }
-})
-->value('id', null);
+})->value('id', null);
 
-
-$app->post('/todo/add', function (Request $request) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
+$app->post('/todo/add', function (Request $request) use ($app)
+{
+    if (null === $user = $app['session']->get('user'))
+    {
         return $app->redirect('/login');
     }
 
-    $errors = $app['validator']->validate($request->get('description'), new Assert\NotBlank());
+    $errors = $app['validator']->validate($request->get('description') , new Assert\NotBlank());
 
-    if (count($errors) == 0) {
+    if (count($errors) == 0)
+    {
 
-    $user_id = $user['id'];
-    $description = $request->get('description');
-    $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
-    $app['db']->executeUpdate($sql);
+        $user_id = $user['id'];
+        $description = $request->get('description');
+        $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
+        $app['db']->executeUpdate($sql);
 
-    # Set flash message and return redirect
-    $app['session']->getFlashBag()->add('success_msg', 'The task has been added successfully.');
-    return $app->redirect('/todo');
-    
-    }
-    else{
-
-    # Set flash message and return redirect
-    $app['session']->getFlashBag()->add('error_msg', 'Description field is required.');
-    return $app->redirect('/todo');
+        # Set flash message and return redirect
+        $app['session']->getFlashBag()
+            ->add('success_msg', 'The task has been added successfully.');
+        return $app->redirect('/todo');
 
     }
-    
+    else
+    {
+
+        # Set flash message and return redirect
+        $app['session']->getFlashBag()
+            ->add('error_msg', 'Description field is required.');
+        return $app->redirect('/todo');
+
+    }
 
 });
 
-
-$app->match('/todo/delete/{id}', function ($id) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
+$app->match('/todo/delete/{id}', function ($id) use ($app)
+{
+    if (null === $user = $app['session']->get('user'))
+    {
         return $app->redirect('/login');
     }
-    
-    $user    = $app['session']->get('user');
+
+    $user = $app['session']->get('user');
     $user_id = $user['id'];
     //$sql = "DELETE FROM todos WHERE id = '$id'";
     $sql = "DELETE FROM todos WHERE id = '$id' AND user_id = '$user_id'";
     $app['db']->executeUpdate($sql);
 
     # Set flash message and return redirect
-    $app['session']->getFlashBag()->add('success_msg', 'The task has been deleted successfully.');
+    $app['session']->getFlashBag()
+        ->add('success_msg', 'The task has been deleted successfully.');
     return $app->redirect('/todo');
 });
 
-$app->match('/todo/edit/{id}', function ($id) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
+$app->match('/todo/edit/{id}', function ($id) use ($app)
+{
+    if (null === $user = $app['session']->get('user'))
+    {
         return $app->redirect('/login');
     }
 
-    $user    = $app['session']->get('user');
+    $user = $app['session']->get('user');
     $user_id = $user['id'];
 
     $sql = "SELECT * FROM todos WHERE id = '$id'";
     $todo = $app['db']->fetchAssoc($sql);
 
-   // print_r($todo);exit;
-
-    return $app['twig']->render('edit.html', [
-            'todo' => $todo,
-    ]);
+    // print_r($todo);exit;
+    return $app['twig']->render('edit.html', ['todo' => $todo, ]);
 
 });
 
-$app->post('/todo/update', function (Request $request) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
+$app->post('/todo/update', function (Request $request) use ($app)
+{
+    if (null === $user = $app['session']->get('user'))
+    {
         return $app->redirect('/login');
     }
 
-    $errors = $app['validator']->validate($request->get('description'), new Assert\NotBlank());
+    $errors = $app['validator']->validate($request->get('description') , new Assert\NotBlank());
 
-    if (count($errors) == 0) {
+    if (count($errors) == 0)
+    {
 
-    $user_id = $user['id'];
-    $description = $request->get('description');
-    $id = $request->get('id');
-    $sql = "UPDATE todos SET description = '$description' WHERE id= '$id'";
-    $app['db']->executeUpdate($sql);
+        $user_id = $user['id'];
+        $description = $request->get('description');
+        $id = $request->get('id');
+        $sql = "UPDATE todos SET description = '$description' WHERE id= '$id'";
+        $app['db']->executeUpdate($sql);
 
-    # Set flash message and return redirect
-    $app['session']->getFlashBag()->add('success_msg', 'The task has been updated successfully.');
-    return $app->redirect('/todo');
-    
-    }
-    else{
-
-    # Set flash message and return redirect
-    $app['session']->getFlashBag()->add('error_msg', 'Description field is required.');
-    return $app->redirect('/todo');
+        # Set flash message and return redirect
+        $app['session']->getFlashBag()
+            ->add('success_msg', 'The task has been updated successfully.');
+        return $app->redirect('/todo');
 
     }
-    
+    else
+    {
+
+        # Set flash message and return redirect
+        $app['session']->getFlashBag()
+            ->add('error_msg', 'Description field is required.');
+        return $app->redirect('/todo');
+
+    }
 
 });
 
-$app->match('/todo/task_complete/{id}', function ($id) use ($app) {
+$app->match('/todo/task_complete/{id}', function ($id) use ($app)
+{
 
-    if (null === $user = $app['session']->get('user')) {
+    if (null === $user = $app['session']->get('user'))
+    {
         return $app->redirect('/login');
     }
 
@@ -193,9 +187,8 @@ $app->match('/todo/task_complete/{id}', function ($id) use ($app) {
     $app['db']->executeUpdate($sql);
 
     # Set flash message and return redirect
-    $app['session']->getFlashBag()->add('success_msg', 'The task has been marked as completed successfully.');
+    $app['session']->getFlashBag()
+        ->add('success_msg', 'The task has been marked as completed successfully.');
     return $app->redirect('/todo');
-    
-    
 
 });
