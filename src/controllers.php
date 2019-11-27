ee8fce0aec687b77e31a0e7271773ea21ab7dbc7
Muhammad Ali <muhammadali@Users-MacBook-Pro.local>
2019-11-27 19:57:22 +1300

    Task1: As a user I can't add a todo without a description.

--- src/controllers.php
+++ src/controllers.php
@@ -1,31 +1,32 @@
 <?php
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
 
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+$app['twig'] = $app->share($app->extend('twig', function ($twig, $app)
+{
     $twig->addGlobal('user', $app['session']->get('user'));
 
     return $twig;
 }));
 
-
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', [
-        'readme' => file_get_contents('README.md'),
-    ]);
+$app->get('/', function () use ($app)
+{
+    return $app['twig']->render('index.html', ['readme' => file_get_contents('README.md') , ]);
 });
 
-
-$app->match('/login', function (Request $request) use ($app) {
+$app->match('/login', function (Request $request) use ($app)
+{
     $username = $request->get('username');
     $password = $request->get('password');
 
-    if ($username) {
+    if ($username)
+    {
         $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
         $user = $app['db']->fetchAssoc($sql);
 
-        if ($user){
+        if ($user)
+        {
             $app['session']->set('user', $user);
             return $app->redirect('/todo');
         }
@@ -34,54 +35,66 @@ $app->match('/login', function (Request $request) use ($app) {
     return $app['twig']->render('login.html', array());
 });
 
-
-$app->get('/logout', function () use ($app) {
+$app->get('/logout', function () use ($app)
+{
     $app['session']->set('user', null);
     return $app->redirect('/');
 });
 
-
-$app->get('/todo/{id}', function ($id) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
+$app->get('/todo/{id}', function ($id) use ($app)
+{
+    if (null === $user = $app['session']->get('user'))
+    {
         return $app->redirect('/login');
     }
+    $user = $app['session']->get('user');
+    $user_id = $user['id'];
 
-    if ($id){
+    if ($id)
+    {
+
         $sql = "SELECT * FROM todos WHERE id = '$id'";
         $todo = $app['db']->fetchAssoc($sql);
 
-        return $app['twig']->render('todo.html', [
-            'todo' => $todo,
-        ]);
-    } else {
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
-        $todos = $app['db']->fetchAll($sql);
-
-        return $app['twig']->render('todos.html', [
-            'todos' => $todos,
-        ]);
+        return $app['twig']->render('todo.html', ['todo' => $todo, ]);
     }
-})
-->value('id', null);
+    else
+    {
 
+        $sql = "SELECT * FROM todos WHERE user_id = '$user_id'";
+        $todos = $app['db']->fetchAll($sql);
+        return $app['twig']->render('todos.html', ['todos' => $todos, ]);
+    }
+})->value('id', null);
 
-$app->post('/todo/add', function (Request $request) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
+$app->post('/todo/add', function (Request $request) use ($app)
+{
+    if (null === $user = $app['session']->get('user'))
+    {
         return $app->redirect('/login');
     }
 
-    $user_id = $user['id'];
-    $description = $request->get('description');
+    $errors = $app['validator']->validate($request->get('description') , new Assert\NotBlank());
 
-    $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
-    $app['db']->executeUpdate($sql);
+    if (count($errors) == 0)
+    {
 
+        $user_id = $user['id'];
+        $description = $request->get('description');
+        $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
+        $app['db']->executeUpdate($sql);
+
+    }
     return $app->redirect('/todo');
+
 });
 
+$app->match('/todo/delete/{id}', function ($id) use ($app)
+{
 
-$app->match('/todo/delete/{id}', function ($id) use ($app) {
-
+    $user = $app['session']->get('user');
+    $user_id = $user['id'];
+    //$sql = "DELETE FROM todos WHERE id = '$id'";
     $sql = "DELETE FROM todos WHERE id = '$id'";
     $app['db']->executeUpdate($sql);
 
